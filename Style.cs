0876a26cacb50f63c51325262d12810356294dbc
codingduo-mb <msteiert@posteo.de>
2021-06-28 21:27:25 +0200

    mbtp-8: fixed textcolor when panel has no tab pages

---
+++ Style.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using static MusicBeePlugin.Plugin;
+
+namespace MusicBeePlugin
+{
+    class Style
+    {
+        private readonly MusicBeeApiInterface mbApiInterface;
+
+        public Style(MusicBeeApiInterface mbApiInterface)
+        {
+            this.mbApiInterface = mbApiInterface;
+        }
+
+        public Color GetElementColor(SkinElement skinElement, ElementState elementState, ElementComponent elementComponent)
+        {
+            //get current skin colors
+            int colorValue = this.mbApiInterface.Setting_GetSkinElementColour(skinElement, elementState, elementComponent);
+            return Color.FromArgb(colorValue);
+        }
+
+        public void StyleControl(Control formControl)
+        {
+            // apply current skin colors to tag panel
+            formControl.Font = this.mbApiInterface.Setting_GetDefaultFont();
+            formControl.BackColor = this.GetElementColor(Plugin.SkinElement.SkinTrackAndArtistPanel, Plugin.ElementState.ElementStateDefault, Plugin.ElementComponent.ComponentBackground);
+            formControl.ForeColor = this.GetElementColor(Plugin.SkinElement.SkinInputControl, Plugin.ElementState.ElementStateDefault, Plugin.ElementComponent.ComponentForeground);
+
+        }
+        
+    }
+}
