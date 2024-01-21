10d088be94c93d6114f6b3c0c27ce37a85774ac4
taxometer <taxometer123@proton.me>
2024-01-21 16:55:39 +0100

    Refactored TagsStorage

--- TagsStorage.cs
+++ TagsStorage.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using static MusicBeePlugin.Plugin;
 
@@ -10,100 +9,50 @@ namespace MusicBeePlugin
     public class TagsStorage
     {
         private string metaDataType;
-        private Dictionary<String, int> tagList = new Dictionary<String, int>();
+        private Dictionary<string, int> tagList = new Dictionary<string, int>();
         private bool sorted = true;
 
-        public TagsStorage()
+        public void Clear() => tagList.Clear();
+
+        public Dictionary<string, CheckState> GetTags()
         {
+            return tagList.ToDictionary(item => item.Key, _ => CheckState.Unchecked);
         }
 
-        public void Clear()
-        {
-            tagList.Clear();
-        }
-
-        internal Dictionary<String, CheckState> GetTags()
-        {
-            Dictionary<String, CheckState> result = new Dictionary<String, CheckState>();
-
-            foreach (var item in tagList)
-            {
-                result.Add(item.Key, CheckState.Unchecked);
-            }
-
-            return result;
-        }
-     
-        public string GetTagName()
-        {
-            return this.metaDataType;
-        }
+        public string GetTagName() => metaDataType;
 
         public MetaDataType GetMetaDataType()
         {
-            return (MetaDataType)Enum.Parse(typeof(MetaDataType), this.metaDataType, true);
+            MetaDataType result;
+            Enum.TryParse(metaDataType, true, out result);
+            return result;
         }
 
         public void Sort()
         {
-            SortedSet<String> sortedKeys = new SortedSet<String>(tagList.Keys);
-            tagList.Clear();
-
-            int idx = 0;
-            foreach (var key in sortedKeys)
-            {
-                tagList.Add(key, idx++);
-            }
-
+            var sortedKeys = new SortedSet<string>(tagList.Keys);
+            tagList = sortedKeys.Select((key, index) => new { Key = key, Index = index })
+                               .ToDictionary(pair => pair.Key, pair => pair.Index);
             sorted = true;
         }
 
         public void SortByIndex()
         {
-            IOrderedEnumerable<KeyValuePair<String, int>> tmpTagList = tagList.OrderBy(item => item.Value);
-
-            Dictionary<String, int> newTagList = new Dictionary<string, int>();
-            
-            foreach (var pair in tmpTagList)
-            {
-                newTagList.Add(pair.Key, pair.Value);
-            }
-
-            tagList.Clear();
-            tagList = newTagList;
+            var tmpTagList = tagList.OrderBy(item => item.Value);
+            tagList = tmpTagList.ToDictionary(pair => pair.Key, pair => pair.Value);
         }
 
-        /// <summary>
-        /// Swaps the element at the old position with the new one.
-        /// </summary>
-        /// <param name="key">String with the existent key.</param>
-        /// <param name="position">The new position.</param>
-        public void SwapElement(String key, int position)
+        public void SwapElement(string key, int position)
         {
             int oldPosition = tagList[key];
             tagList[key] = position;
 
-            tagList[FilterPosition(tagList, position)] = oldPosition;
+            string oldKey = tagList.FirstOrDefault(item => item.Value == position).Key;
+            tagList[oldKey] = oldPosition;
         }
 
-        private String FilterPosition(Dictionary<String, int> list, int value)
-        {
-            String newKey = "";
-            foreach (var item in list)
-            {
-                if (item.Value == value)
-                {
-                    newKey = item.Key;
-                    break;
-                }
-            }
-            return newKey;
-        }
-                
         public bool Sorted { get => sorted; set => sorted = value; }
         public string MetaDataType { get => metaDataType; set => metaDataType = value; }
         public Dictionary<string, int> TagList { get => tagList; set => tagList = value; }
     }
 }
-
-
