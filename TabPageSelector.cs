bcf8cae4b9a7b051682649fc10c8741c46c15120
taxometer <taxometer123@proton.me>
2024-02-04 23:10:09 +0100

    Refactored TabPageSelector

--- TabPageSelector.cs
+++ TabPageSelector.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using static MusicBeePlugin.Plugin;
 
@@ -13,24 +9,24 @@ namespace MusicBeePlugin
     public partial class TabPageSelectorForm : Form
     {
         // Tags in this list will be ignored in the TagSelector form
-        readonly HashSet<string> blacklist = new HashSet<string> { 
-            MetaDataType.Artwork.ToString("g"),
-            MetaDataType.DiscNo.ToString("g"),
-            MetaDataType.DiscCount.ToString("g"),
-            MetaDataType.Encoder.ToString("g"),
-            MetaDataType.HasLyrics.ToString("g"),
-            MetaDataType.Lyrics.ToString("g"),
-            MetaDataType.TrackCount.ToString("g"),
-            MetaDataType.Rating.ToString("g"),
-            MetaDataType.RatingAlbum.ToString("g"),
-            MetaDataType.RatingLove.ToString("g"),
-        };
+        private static readonly HashSet<MetaDataType> blacklist = new HashSet<MetaDataType> {
+                MetaDataType.Artwork,
+                MetaDataType.DiscNo,
+                MetaDataType.DiscCount,
+                MetaDataType.Encoder,
+                MetaDataType.HasLyrics,
+                MetaDataType.Lyrics,
+                MetaDataType.TrackCount,
+                MetaDataType.Rating,
+                MetaDataType.RatingAlbum,
+                MetaDataType.RatingLove
+            };
 
         public TabPageSelectorForm()
         {
             InitializeComponent();
-            this.Btn_ComboBoxAddTag.DialogResult = DialogResult.OK;
-            this.Btn_ComboBoxCancel.DialogResult = DialogResult.Cancel;
+            Btn_ComboBoxAddTag.DialogResult = DialogResult.OK;
+            Btn_ComboBoxCancel.DialogResult = DialogResult.Cancel;
             SetMetaDataTypes();
         }
 
@@ -42,29 +38,19 @@ namespace MusicBeePlugin
 
         private List<string> GetMetaDataTypesAsString()
         {
-            Array values = Enum.GetValues(typeof(MetaDataType));
-            List<string> dataTypesAsString = new List<string>();
-            foreach (MetaDataType dataType in values)
-            {
-                string asString = dataType.ToString("g");
-                if (blacklist.Contains(asString))
-                {
-                    continue;
-                }
-                dataTypesAsString.Add(asString);
-            }
+            List<string> dataTypesAsString = Enum.GetValues(typeof(MetaDataType))
+                .Cast<MetaDataType>()
+                .Where(dataType => !blacklist.Contains(dataType))
+                .Select(dataType => dataType.ToString("g"))
+                .ToList();
+
             dataTypesAsString.Sort();
             return dataTypesAsString;
         }
 
         public string GetMetaDataType()
         {
-            object selectedItem = comboBoxTagSelect.SelectedItem;
-            if (selectedItem != null)
-            {
-                return (string) selectedItem;
-            }
-            return null;
+            return comboBoxTagSelect.SelectedItem as string;
         }
     }
 }
