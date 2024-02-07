b32571c37fcbd121b1d02647110b453d66907c54
taxometer <taxometer123@proton.me>
2024-02-07 13:02:44 +0100

    Small impriovments in ChecklistBoxPanel

--- ChecklistBoxPanel.cs
+++ ChecklistBoxPanel.cs
@@ -10,7 +10,6 @@ namespace MusicBeePlugin
     {
         private readonly MusicBeeApiInterface mbApiInterface;
         private ItemCheckEventHandler eventHandler;
-
         private Style style;
 
         public ChecklistBoxPanel(MusicBeeApiInterface mbApiInterface, Dictionary<String, CheckState> data = null)
@@ -20,60 +19,64 @@ namespace MusicBeePlugin
 
             InitializeComponent();
 
-
             if (data != null)
             {
                 AddDataSource(data);
             }
+
             StylePanel();
         }
 
         public void AddDataSource(Dictionary<String, CheckState> data)
         {
-            this.checkedListBox1.BeginUpdate(); // Suspend the drawing of the control
-            this.checkedListBox1.Items.Clear();
+            checkedListBox1.BeginUpdate();
+            checkedListBox1.Items.Clear();
 
-            string longestString = data.Keys.Aggregate("", (max, cur) => cur.Length > max.Length ? cur : max);
             foreach (KeyValuePair<String, CheckState> entry in data)
             {
-                this.checkedListBox1.Items.Add(entry.Key, entry.Value);
+                checkedListBox1.Items.Add(entry.Key, entry.Value);
             }
 
-            this.checkedListBox1.ColumnWidth = TextRenderer.MeasureText(longestString, checkedListBox1.Font).Width + 20;
-            this.checkedListBox1.EndUpdate(); // Resume the drawing of the control
+            checkedListBox1.ColumnWidth = GetLongestStringWidth(data.Keys) + 20;
+            checkedListBox1.EndUpdate();
         }
 
+        private int GetLongestStringWidth(IEnumerable<string> strings)
+        {
+            var longestString = strings.Aggregate("", (max, cur) => cur.Length > max.Length ? cur : max);
+            return TextRenderer.MeasureText(longestString, checkedListBox1.Font).Width;
+        }
 
         private void StylePanel()
         {
             // apply current skin colors to tag panel
-            this.style.StyleControl(checkedListBox1);
-            this.style.StyleControl(this);
+            style.StyleControl(checkedListBox1);
+            style.StyleControl(this);
         }
 
 
         public void AddItemCheckEventHandler(ItemCheckEventHandler eventHandler)
         {
             this.eventHandler = eventHandler;
-            this.checkedListBox1.ItemCheck += eventHandler;
+            checkedListBox1.ItemCheck += eventHandler;
         }
 
         public void RemoveItemCheckEventHandler()
         {
             //RemoveClickEvent(this.checkedListBox1);
-            this.checkedListBox1.ItemCheck -= this.eventHandler;
+            checkedListBox1.ItemCheck -= this.eventHandler;
         }
 
         private void CheckedListBox1_KeyUp(object sender, KeyEventArgs e)
         {
             // this will prevent the item to be checked if a key was pressed
-            this.checkedListBox1.CheckOnClick = true;
+            checkedListBox1.CheckOnClick = true;
         }
 
         private void CheckedListBox1_KeyDown(object sender, KeyEventArgs e)
         {
             // this will prevent the item to be checked if a key was pressed
-            this.checkedListBox1.CheckOnClick = false;
+            checkedListBox1.CheckOnClick = false;
         }
     }
 }
